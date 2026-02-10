963e9285525617395455d052feedf6b8547f7c2e
ChahatPoptani <chahatpoptani13@gmail.com>
2026-02-11 00:27:56 +0530

    Setup frontend env and axios for backend integration

---
+++ src/api/auth.ts
@@ -0,0 +1,18 @@
+import api from "./axios";
+
+export const loginUser = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const res = await api.post("/api/auth/login", data);
+  return res.data;
+};
+
+export const registerUser = async (data: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+  const res = await api.post("/api/auth/register", data);
+  return res.data;
+};
